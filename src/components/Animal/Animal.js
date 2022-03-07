09297b2cc3a4206b722662720869a8a6019fa0e5
Cole Rossman <colerossman99@gmail.com>
2022-03-07 14:09:54 -0800

    main.js and animal.js progress

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import './Animal.css';
 
-export default function Animal() {
-  return <></>;
+export default function Animal(info) {
+  return (
+    <div className='animal'>
+      <img style={{ top: (info.top), left: (info.left) }} src={`${process.env.PUBLIC_URL}/animals-${info.type}.svg`} />
+      <p></p>
+      
+    </div>
+  );
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import './Main.css';
+import { animals } from '../../data.js';
+import background from '../../background.png';
+import Animal from '../Animal/Animal';
 
 export default function Main() {
-  return <main></main>;
+  return <main className='App' style={{ backgroundImage: `url(${background})` }} >
+    {animals.map((animal) => (
+      <Animal key={animal.name} type={animal.type} says={animal.says} top={animal.top} left={animal.left} />
+    ))};
+  </main>;
 }
