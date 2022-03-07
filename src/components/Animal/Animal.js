bb617c5217c89d1ff6c7206d339b3f3d7a3fea2d
Cole Rossman <colerossman99@gmail.com>
2022-03-07 14:51:56 -0800

    added classname to span to change name appearance

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './Animal.css';
 
 export default function Animal({ animal }) {
-  console.log(animal);
+  // console.log(animal);
   return (
     <div className='animal' style={{ top: animal.top, left: animal.left }}>
       <img src={`${process.env.PUBLIC_URL}/animals/${animal.type}.svg`} />
-      <span>{animal.name}</span>
+      <span className='name'>{animal.name}</span>
       <span>{animal.says}</span>
-      
+
     </div>
   );
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -7,7 +7,6 @@ import Animal from '../Animal/Animal';
 export default function Main() {
   return <main className='App' style={{ backgroundImage: `url(${background})` }} >
     {animals.map((animal) => (
-      // <Animal key={animal.name} name={animal.name} type={animal.type} says={animal.says} top={animal.top} left={animal.left} />
       <Animal key={animal.name} animal={animal} />
     ))};
   </main>;
